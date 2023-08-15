eddedfdcdaf7812d9649cbdc9502f27f4c99df05
Michyjones <mykembuguah11@gmail.com>
2023-08-15 13:03:37 +0300

    Addedstripe payment function to fulfil an order

--- ecommerce-admin/lib/utils.ts
+++ ecommerce-admin/lib/utils.ts
@@ -10,15 +10,4 @@ export const formatter = new Intl.NumberFormat("en-US", {
   currency: "KES",
 });
 
-// export const formatMoney = (price: number = 0): string => {
-//   const options: Intl.NumberFormatOptions = {
-//     style: 'currency',
-//     currency: 'KES',
-//     minimumFractionDigits: 2,
-//   };
-//   if (price % 100 === 0) {
-//     options.minimumFractionDigits = 0;
-//   }
-//   const formatter = new Intl.NumberFormat('en-KE', options);
-//   return formatter.format(price);
-// };
+
